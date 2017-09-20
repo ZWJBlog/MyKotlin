12c287bbdbac5a7bd7204fd319bcc0efc2ceb9f4
ZwjBlog <1647454139@qq.com>
2017-09-21 00:26:01 +0800

    更新demo

--- app/src/main/java/com/example/msi/mykotlin/Main2Activity.kt
+++ app/src/main/java/com/example/msi/mykotlin/Main2Activity.kt
@@ -9,7 +9,7 @@ class Main2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         var btn = findViewById(R.id.btn)
-        btn.setOnClickListener {   Toast.makeText(this, "sum is 12344", 1).show();  }
+        btn.setOnClickListener { Toast.makeText(this, "sum is 12344", 1).show(); }
         val sum = sum(1, 2);
         println("sum is ${sum(1, 2)}");
 
@@ -17,6 +17,17 @@ class Main2Activity : AppCompatActivity() {
         for (value in array) {
             println("value = $value")
         }
+        var sums:List<String> = listOf<String>()
+        for (a:Int in 1..100) {
+            sums.indexOf(""+a);
+            sums.plus(""+a)
+        }
+        val numbers: MutableList<String> = mutableListOf()
+        for (a:Int in 1..100) {
+            numbers.add(""+a)
+        }
+
+        Toast.makeText(this, ""+sums.size, 1).show();
     }
 
     fun sum(a: Int, b: Int): Int {
